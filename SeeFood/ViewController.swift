aa765ac01d78f8cfceeb15d5c61e2e91adecbbdd
fatimbow <fatmabasaran217@gmail.com>
2023-07-13 18:39:38 +0300

    Classify images as "Hotdog!" and "Not Hotdog!"

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -34,9 +34,41 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 fatalError("Could not convert to CIImage.")
             }
             
+            detect(image: ciImage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
